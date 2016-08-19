ce65b4a3588494e7362b667459ea3bf20ff09ec5
hielf <hielf@qq.com>
2016-08-19 17:18:18 +0800

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 admin = Admin.create!(
   name: "foobar",
   password: "111111")
-user = User.create(
-  openid: Faker::Number.number(10).to_s,
-  avatar: File.open(File.join(Rails.root, 'spec/fixtures/rails.png')),
-  name: "foobar",
-  cell: "11111111111",
-  email: Faker::Internet.email,
-  id_card: "ID card",
-  bank_card: "Bank card",
-  alipay: "alipay"
-  )
+# user = User.create(
+#   openid: Faker::Number.number(10).to_s,
+#   avatar: File.open(File.join(Rails.root, 'spec/fixtures/rails.png')),
+#   name: "foobar",
+#   cell: "11111111111",
+#   email: Faker::Internet.email,
+#   id_card: "ID card",
+#   bank_card: "Bank card",
+#   alipay: "alipay"
+#   )
