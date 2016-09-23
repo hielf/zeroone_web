8f1f59786b05ce1f32a86ceed62215d9330186cd
hielf <hielf@qq.com>
2016-09-23 14:24:30 +0800

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include ApplicationHelper
-  before_action :logged_in
-
-  private
-    def logged_in
-      unless (admin_logged_in? || user_logged_in?)
-        flash[:danger] = t(:please_log_in)
-        redirect_to user_login_path
-      end
-    end
 end
