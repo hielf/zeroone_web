344210506d04607023d067eeefbd00a793caddb0
hielf <hielf@qq.com>
2016-10-11 03:19:09 +0800

    commit

--- app/controllers/api/records_controller.rb
+++ app/controllers/api/records_controller.rb
@@ -1,5 +1,5 @@
 class Api::RecordsController < Api::BaseController
-  # skip_before_action :authenticate_user!, only: [:create, :send_code]
+  skip_before_action :authenticate_user!, only: [:notify]
   def notify
     Rails.logger.warn "notify #{Time.now}, sign: #{params[:sign]};"
   end
