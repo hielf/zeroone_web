5ad84c239bce9876baffc628531f235e5b953088
hielf <hielf@qq.com>
2016-10-13 04:00:38 +0800

    commit

--- db/migrate/20161012192318_add_days_to_records.rb
+++ db/migrate/20161012192318_add_days_to_records.rb
@@ -1,6 +1,6 @@
 class AddDaysToRecords < ActiveRecord::Migration
   def change
-    add_column :records, :total_insured, :number
+    add_column :records, :total_insured, :decimal, precision: 12, scale: 2, default: 0
     add_column :records, :start_date, :date
     add_column :records, :end_date, :date
     add_column :records, :policy_no, :string
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930054527) do
+ActiveRecord::Schema.define(version: 20161012192318) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 20160930054527) do
     t.string   "status",        limit: 255
     t.datetime "created_at",                                                       null: false
     t.datetime "updated_at",                                                       null: false
+    t.decimal  "total_insured",             precision: 12, scale: 2, default: 0.0
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "policy_no",     limit: 255
   end
 
   create_table "users", force: :cascade do |t|
