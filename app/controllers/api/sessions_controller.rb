0c806cb51daad50c121fd818d63d577dcf9d4069
hielf <hielf@qq.com>
2016-11-25 16:44:04 +0800

    commit

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::SessionsController < Api::BaseController
     @user = User.find_by(cell: params[:cell])
     if @user && (@user.authenticate(params[:password]) || CellCode.find_by(cell: @user.cell, code: params[:code]))
       @user.update(openid: params[:openid]) if params[:openid]
-      render json: {token: @user.token, cell: @user.cell}
+      render json: {token: @user.token, superior: @user.superior.name if (@user.user_type == "channel"), cell: @user.cell}
     elsif CellCode.find_by(cell: params[:cell], code: params[:code])
       @user = User.find_by(openid: params[:openid])
       if @user
@@ -13,7 +13,7 @@ class Api::SessionsController < Api::BaseController
       else
         @user = User.create(cell: params[:cell], code: params[:code], openid: params[:openid], password: rand(1000..9999).to_s)
       end
-      render json: {token: @user.token, cell: @user.cell}
+      render json: {token: @user.token, superior: @user.superior.name if (@user.user_type == "channel"), cell: @user.cell}
     else
       return api_error(status: 401)
     end
